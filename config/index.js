4b41cc2b72baf5d366d45ca21ab4b731d4ccd814
e.shangin <evgeniy.shangin@uchi.ru>
2017-12-24 17:33:37 +0300

    test static

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/js_testing/',
     proxyTable: {},
 
     // Various Dev Server settings
--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -7,7 +7,16 @@
       @click="$emit('change-question', num)"
       :class="{ paginator__item_current: currentQuestionIndex === num }"
     >
-      {{ num }}
+      <span class="paginator__rate" v-if="toggleRate(num)">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="1em" height="1em" viewBox="0 0 442.533 442.533" style="enable-background:new 0 0 442.533 442.533;" xml:space="preserve">
+          <path fill="#5eb42e" d="M434.539,98.499l-38.828-38.828c-5.324-5.328-11.799-7.993-19.41-7.993c-7.618,0-14.093,2.665-19.417,7.993L169.59,247.248
+            l-83.939-84.225c-5.33-5.33-11.801-7.992-19.412-7.992c-7.616,0-14.087,2.662-19.417,7.992L7.994,201.852
+            C2.664,207.181,0,213.654,0,221.269c0,7.609,2.664,14.088,7.994,19.416l103.351,103.349l38.831,38.828
+            c5.327,5.332,11.8,7.994,19.414,7.994c7.611,0,14.084-2.669,19.414-7.994l38.83-38.828L434.539,137.33
+            c5.325-5.33,7.994-11.802,7.994-19.417C442.537,110.302,439.864,103.829,434.539,98.499z"/>
+        </svg>
+      </span>
+      <span class="paginator__num" v-else>{{ num }}</span>
     </div>
   </div>
 </template>
@@ -21,7 +30,14 @@ export default {
     ...mapGetters([
       'currentQuestionIndex',
       'arrQuestionsIndexes',
+      'picked',
+      'allRatings',
     ])
+  },
+  methods: {
+    toggleRate(idx) {
+      return this.picked === '1' && this.allRatings[idx] != null
+    }
   }
 }
 </script>
@@ -37,9 +53,13 @@ export default {
 }
 .paginator__item {
   $border-width: 0.2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 0.5em;
-  padding: 0.4em 1.8em;
   border-radius: 7px;
+  width: 5em;
+  height: 2.2em;
   background-color: lightblue;
   box-sizing: border-box;
   color: $vue-blue;
