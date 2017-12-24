27a7f25bf11070bb706e74007c5ba10670b9bb17
e.shangin <evgeniy.shangin@uchi.ru>
2017-12-24 17:36:57 +0300

    test static

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/js_testing/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
